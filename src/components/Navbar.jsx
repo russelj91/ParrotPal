a7011f74f2603d5c57770b95c2d39a413c4c0a05
russelj91 <email>
2023-09-21 22:53:30 +0800

    giftIcon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,14 @@ const Navbar = () => {
         className={`navbar navbar-expand-lg navbar-dark text-center shadow-sm ${styles.navbar}`}
       >
         <div className="container">
-          <a className="navbar-brand" href="https://www.parrotpal.com/">
+          <Image
+            src="/LandingPageAssets/Images/gift2.png"
+            alt="Analytics"
+            className={`img-fluid d-block d-md-none ${styles.img}`}
+            width={55}
+            height={55}
+          />
+          <a className="navbar-brand mx-auto" href="https://www.parrotpal.com/">
             <Image
               src="/LandingPageAssets/Images/NewLogo.png"
               alt="ParrotPal Logo"
@@ -34,7 +41,7 @@ const Navbar = () => {
             className={`collapse navbar-collapse ${styles.navbarCollapse}`}
             id="navbarSupportedContent"
           >
-            <ul className="navbar-nav mx-auto ">
+            <ul className="navbar-nav mx-auto">
               <li className={`nav-item ${styles.dropLink}`}>
                 <a
                   className={`nav-link fw-bold ${styles.archor}`}
@@ -51,6 +58,13 @@ const Navbar = () => {
               <li className={`nav-item ${styles.dropLink}`}>
                 <a className={`nav-link  fw-bold ${styles.archor}`} href="#">
                   About Us
+                </a>
+              </li>
+              <li
+                className={`nav-item d-none d-lg-inline d-md-block d-block ${styles.dropLink}`}
+              >
+                <a className={`nav-link  fw-bold ${styles.archor}`} href="#">
+                  Gift Parrot<span className={styles.parrot}>Pal</span>
                 </a>
               </li>
             </ul>
